dd773047e7113b18e259a0b7d9e466da023538bb
dimsand <dimitri.sandron@outlook.com>
2015-06-16 12:24:27 +0200

    modification login.php

--- login.php
+++ login.php
@@ -9,10 +9,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 
 </html>
 
 <?php
+
+
+?>
 
